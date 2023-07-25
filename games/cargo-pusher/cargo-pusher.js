5a319aff934a35881b94fbc9737362464c5d6e52
MB3429 <brandzohar4@gmail.com>
2023-07-24 17:04:09 -0700

    Working on Cargo Pusher

--- games/cargo-pusher/cargo-pusher.js
+++ games/cargo-pusher/cargo-pusher.js
@@ -4,9 +4,8 @@ context.imageSmoothingEnabled = false;
 const width = canvas.width;
 const height = canvas.height;
 
-let curScreen = 'start';
+let curScreen = 'levels';
 let curLevel = undefined;
-let settingsOpen = false;
 
 const spriteList = document.getElementsByClassName('sprite');
 const iconList = document.getElementsByClassName('icon');
@@ -15,11 +14,10 @@ let pushAmount = 0;
 let pushable = [];
 
 let savedData = JSON.parse(localStorage.getItem('cargo-pusher')) || {
-  lastLevelUnlocked: 1,
-  volume: 2,
-  mobileMode: false
+  lastLevelUnlocked: 1
 };
 localStorage.setItem('cargo-pusher', JSON.stringify(savedData));
+let mobileMode
 
 let mouseX = 0;
 let mouseY = 0;
@@ -27,8 +25,6 @@ const clickableRegions = [];
 let iconSize;
 let clickX = -100;
 let clickY = -100;
-let settingsClickX = clickX;
-let settingsClickY = clickY;
 
 let transition = undefined;
 let transitionIn = undefined;
@@ -42,19 +38,6 @@ setInterval(tickGame, 50);
 function tickGame() {
   clickableRegions.length = 0;
   context.clearRect(0, 0, width, height)
-
-  settingsClickX = clickX;
-  settingsClickY = clickY;
-
-  if (settingsOpen &&
-    clickX > 440 &&
-    clickX < 630 &&
-    clickY > 10 &&
-    clickY < 135
-  ) {
-    clickX = -100;
-    clickY = -100;
-  }
 
   switch (curScreen) {
     case 'start': {
@@ -79,7 +62,6 @@ function tickGame() {
   
   // Handles transition fade-outs
   if (transition) {
-    settingsOpen = false;
     if (transition.frame > 0) {
       transition.frame--;
       if (transition.frame < 10) {
@@ -103,82 +85,56 @@ function tickGame() {
     }
   }
 
-  settingsScreen();
-
   clickX = -100;
   clickY = -100;
   input = undefined;
 }
 
-// Handles the settings button and settings screen
-function settingsScreen() {
-  iconSize = [width-67,15,52,52];
-  if (regionContains(settingsClickX, settingsClickY, ...iconSize)) settingsOpen = !settingsOpen;
-
-  if (settingsOpen) {
-    context.lineWidth = 4;
-    const settingsWidth = 190;
-    const settingsHeight = 125;
-    context.clearRect(630-settingsWidth,10,settingsWidth,settingsHeight);
-    context.strokeRect(630-settingsWidth,10,settingsWidth,settingsHeight);
-    context.drawImage(iconList[0],13,0,13,13,...iconSize);
-
-    // Change volume button
-    iconSize = [640-settingsWidth,15,52,52];
-    if (regionContains(settingsClickX, settingsClickY, ...iconSize)) {
-      savedData.volume = (savedData.volume + 1) % 3;
-      localStorage.setItem('cargo-pusher', JSON.stringify(savedData));
-    }
-    context.drawImage(iconList[1],savedData.volume * 13,0,13,13,...iconSize);
-
-    // Toggle Mobile/PC button
-    iconSize = [630-11*settingsWidth/12,80,5*settingsWidth/6,40];
-    context.strokeRect(...iconSize);
-    if (regionContains(settingsClickX, settingsClickY, ...iconSize)) {
-      savedData.mobileMode = !savedData.mobileMode;
-      localStorage.setItem('cargo-pusher', JSON.stringify(savedData));
-    }
-    context.fillStyle = 'black';
-    context.textAlign = 'center';
-    context.font = '24px "Press Start 2P"';
-    context.fillText(savedData.mobileMode ? 'Mobile' : 'PC',630-settingsWidth/2,114);
-
-  } else {
-    context.drawImage(iconList[0],0,0,13,13, ...iconSize)
-  }
-}
-
 // Handles the start screen of the game
 function startScreen() {
-  iconSize = [width/2-150, height/2+50, 300, 60];
+  context.fillStyle = 'black';
+  context.textAlign = 'center';
+  context.font = '40px "Press Start 2P"';
+  context.lineWidth = 4;
+
+  context.fillText('Cargo Pusher', width/2, height/2-90);
+
+  iconSize = [width/2-150, height/2+10, 300, 60];
   if (regionContains(clickX, clickY, ...iconSize)) {
     transition = {
       frame: 25,
       endScreen: 'levels'
     };
+    mobileMode = false;
   }
 
-  context.lineWidth = 4;
   context.strokeRect(...iconSize);
+  context.fillText('PC', width/2, height/2+63);
 
-  context.fillStyle = 'black';
-  context.textAlign = 'center';
-  context.font = '40px "Press Start 2P"';
-  context.fillText('Play', width/2, height/2+103);
-  context.fillText('Cargo Pusher', width/2, height/2-50);
+  iconSize = [width/2-150, height/2+90, 300, 60];
+  if (regionContains(clickX, clickY, ...iconSize)) {
+    transition = {
+      frame: 25,
+      endScreen: 'levels'
+    };
+    mobileMode = true;
+  }
 
-  context.drawImage(spriteList[6],0,0,32,32,width/2,height/2-32,64,64);
-  context.drawImage(spriteList[1],width/2+64,height/2-32,64,64)
-  context.drawImage(spriteList[8],0,0,32,32,width/2+64,height/2-32,64,64);
+  context.strokeRect(...iconSize);
+  context.fillText('Mobile', width/2, height/2+143);
+  
+  context.drawImage(spriteList[6],0,0,32,32,width/2,height/2-72,64,64);
+  context.drawImage(spriteList[1],width/2+64,height/2-72,64,64)
+  context.drawImage(spriteList[8],0,0,32,32,width/2+64,height/2-72,64,64);
 
   let offset = 0;
   if (transition) offset = Math.min((25-transition.frame)*64/15,64);
 
-  context.drawImage(spriteList[1],width/2-128,height/2-32,64,64)
-  context.drawImage(spriteList[1],width/2-64,height/2-32,64,64)
+  context.drawImage(spriteList[1],width/2-128,height/2-72,64,64)
+  context.drawImage(spriteList[1],width/2-64,height/2-72,64,64)
 
-  context.drawImage(spriteList[0],0,0,32,32,width/2-128+offset,height/2-32,64,64);
-  context.drawImage(spriteList[2],width/2-64+offset,height/2-32,64,64);
+  context.drawImage(spriteList[0],0,0,32,32,width/2-128+offset,height/2-72,64,64);
+  context.drawImage(spriteList[2],width/2-64+offset,height/2-72,64,64);
 }
 
 // Handles the level select screen of the game
@@ -225,7 +181,7 @@ function inLevelScreen() {
   printLayer(curLvlData.floor, lvlWidth, lvlHeight, 32);
   printLayer(curLvlData.items, lvlWidth, lvlHeight, 32);
   printPlayer(lvlWidth, lvlHeight, 32);
-  if (savedData.mobileMode) printMovement(lvlWidth, lvlHeight, 32);
+  if (mobileMode) printMovement(lvlWidth, lvlHeight, 32);
 
   context.font = '32px "Press Start 2P"';
   context.fillStyle = 'black';
@@ -609,7 +565,7 @@ canvas.addEventListener('mousemove', Event => {
   mouseX = (Event.clientX - bounding.x)/canvas.offsetWidth*640;
   mouseY = (Event.clientY - bounding.y)/canvas.offsetHeight*480;
   canvas.style.cursor = 'default';
-  if (!savedData.mobileMode && !transition && !transitionIn &&
+  if (!mobileMode && !transition && !transitionIn &&
     clickableRegions.find(region => regionContains(mouseX,mouseY,...region, false))
   ) {
     canvas.style.cursor = 'pointer';
@@ -626,7 +582,7 @@ canvas.addEventListener('click', () => {
 
 // Detects key presses and converts them to player moves
 canvas.addEventListener('keydown', Event => {
-  if (transition || transitionIn || savedData.mobileMode) return;
+  if (transition || transitionIn || mobileMode) return;
   switch(Event.key.toLowerCase()) {
     case 'w':
     case 'arrowup': {
